e636a521a4cc956e6e341cafee72ad8b466b798b
Alexa Urrea <alexa.urrea98.com>
2021-11-30 12:56:05 -0700

    Saved docs

---
+++ creative-project3/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div id="brand">
+        <router-link to="/">
+          <img src="">
+        </router-link>
+      </div>
+      <div id="side">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/showing">Now Showing</router-link> |
+        <router-link to="/browse">Browse</router-link> |
+        <router-link to="/about">About</router-link>
+        </div>
+    </div>
+    <router-view/>
+    <div class="footer">
+      <a href="https://github.com/Lebam360/creativeProject-3">Github Repsitory</a>
+      <br> <a href="http://anyaanderson.com">Link back to Anya Anderson's Website</a>
+      <br> <a href="http://alexauferg.com/">Link back to Alexa Urrea's Website</a>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Papyrus, fantasy;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #BEBEBE;
+}
+
+body {
+  background: #003645;
+}
+
+#nav {
+  padding: 30px;
+  background-color: #800000;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #BEBEBE;
+}
+
+#nav a.router-link-exact-active {
+  color: #786AD8;
+}
+.footer {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  height: auto;
+  width: 100%;
+  text-align: center;
+  color: #6b87d8;
+  background-color: #800000;
+}
+.footer a {
+  color: #FAF9F6;
+}
+
+</style>
---
+++ creative-project3/src/MOCK_DATA.js
@@ -0,0 +1,508 @@
+let mock = [
+{
+  id:1,
+  title:"Kissed",
+  director_last_name:"Chilver",
+  company_name:"Disney",
+  genre:"Drama|Romance",
+  day_of_the_week:"Thursday",
+  time:"10:00 AM",
+  image:"kissed.jpg"
+},
+{
+  id:2,
+  title:"Nosferatu (Nosferatu, eine Symphonie des Grauens)",
+  director_last_name:"Larvin",
+  company_name:"Disney",
+  genre:"Horror",
+  day_of_the_week:"Saturday",
+  time:"10:30 AM",
+  image:"nosferatu.jpg"
+},
+{
+  id:3,
+  title:"Sekirei",
+  director_last_name:"Razzell",
+  company_name:"Paramount",
+  genre:"(no genres listed)",
+  day_of_the_week:"Tuesday",
+  time:"10:30 AM",
+  image:"sekirei.jpg"
+},
+{
+  id:4,
+  title:"War Lord",
+  director_last_name:"Emberton",
+  company_name:"Paramount",
+  genre:"Drama|Romance|War",
+  day_of_the_week:"Friday",
+  time:"11:30 AM",
+  image:"war-lord.jpg"
+},
+{
+  id:5,
+  title:"Punchline",
+  director_last_name:"Strank",
+  company_name:"Disney",
+  genre:"Comedy|Drama",
+  day_of_the_week:"Saturday",
+  time:"12:00 PM",
+  image:"punchline.jpg"
+},
+{
+  id:6,
+  title:"Game of Chance (Onnenpeli)",
+  director_last_name:"Di Ruggero",
+  company_name:"Paramount",
+  genre:"Comedy|Drama",
+  day_of_the_week:"Saturday",
+  time:"12:30 PM",
+  image:"game-chance.jpg"
+},
+{
+  id:7,
+  title:"Witness to Murder",
+  director_last_name:"Wedge",
+  company_name:"Paramount",
+  genre:"Crime|Drama|Film-Noir|Thriller",
+  day_of_the_week:"Saturday",
+  time:"1:00 PM",
+  image:"witness-murder.jpg"
+},
+{
+  id:8,
+  title:"White Men Can't Jump",
+  director_last_name:"Yerson",
+  company_name:"Universal Pictures",
+  genre:"Comedy|Drama",
+  day_of_the_week:"Sunday",
+  time:"1:30 PM",
+  image:"white-men.jpg"
+},
+{
+  id:9,
+  title:"Toy",
+  director_last_name:"Yeskov",
+  company_name:"Sony Pictures",
+  genre:"Comedy",
+  day_of_the_week:"Saturday",
+  time:"2:00 PM",
+  image:"toy.jpg"
+},
+{
+  id:10,
+  title:"Summer Days With Coo",
+  director_last_name:"Maciaszek",
+  company_name:"Paramount",
+  genre:"Animation",
+  day_of_the_week:"Thursday",
+  time:"2:30 PM",
+  image:"summer-days.jpg"
+},
+{
+  id:11,
+  title:"Devil's Carnival",
+  director_last_name:"Fulmen",
+  company_name:"Warner Bros.",
+  genre:"Horror|Musical",
+  day_of_the_week:"Tuesday",
+  time:"2:30 PM",
+  image:"devils-carnival.jpg"
+},
+{
+  id:12,
+  title:"Shakespeare High",
+  director_last_name:"Cater",
+  company_name:"Pixar",
+  genre:"Documentary",
+  day_of_the_week:"Saturday",
+  time:"2:30 PM",
+  image:"shakespeare-high.jpg"
+},
+{
+  id:13,"title":"Je m'appelle Elisabeth",
+  director_last_name:"O'Kennedy",
+  company_name:"Sony Pictures",
+  genre:"(no genres listed)",
+  day_of_the_week:"Wednesday",
+  time:"3:00 PM",
+  image:"elisabeth.jpg"
+},
+{
+  id:14,
+  title:"Fireflies in the Garden",
+  director_last_name:"Lewinton",
+  company_name:"Warner Bros.",
+  genre:"Drama",
+  day_of_the_week:"Wednesday",
+  time:"3:00 PM",
+  image:"fireflies.jpg"
+},
+{
+  id:15,
+  title:"Stand Up and Fight",
+  director_last_name:"Wordesworth",
+  company_name:"Pixar",
+  genre:"Drama|Romance|Western",
+  day_of_the_week:"Tuesday",
+  time:"3:00 PM",
+  image:"stand-up.jpg"
+},
+{
+  id:16,
+  title:"Shadows (Cienie)",
+  director_last_name:"Ballintyne",
+  company_name:"Pixar",
+  genre:"Drama",
+  day_of_the_week:"Wednesday",
+  time:"3:00 PM",
+  image:"shadows.jpg"
+},
+{
+  id:17,
+  title:"Ricky Rapper (Risto Räppääjä)",
+  director_last_name:"Mc Curlye",
+  company_name:"Paramount",
+  genre:"Comedy|Musical",
+  day_of_the_week:"Saturday",
+  time:"3:30 PM",
+  image:"ricky-rapper.jpg"
+},
+{
+  id:18,
+  title:"Darkman II: Return of Durant",
+  director_last_name:"Gregg",
+  company_name:"Universal Pictures",
+  genre:"Action|Crime|Horror",
+  day_of_the_week:"Tuesday",
+  time:"4:00 PM",
+  image:"darkman.jpg"
+},
+{
+  id:19,
+  title:"Sting",
+  director_last_name:"Hodgets",
+  company_name:"Pixar",
+  genre:"Comedy|Crime",
+  day_of_the_week:"Thursday",
+  time:"4:00 PM",
+  image:"sting.jpg"
+},
+{
+  id:20,
+  title:"Shrooms",
+  director_last_name:"Lavarack",
+  company_name:"Disney",
+  genre:"Horror|Thriller",
+  day_of_the_week:"Wednesday",
+  time:"4:00 PM",
+  image:"shrooms.jpg"
+},
+{
+  id:21,
+  title:"Like Water",
+  director_last_name:"Sheldrick",
+  company_name:"Disney",
+  genre:"Documentary",
+  day_of_the_week:"Tuesday",
+  time:"4:00 PM",
+  image:"like-water.jpg"
+},
+{
+  id:22,
+  title:"Dracula",
+  director_last_name:"Moloney",
+  company_name:"Paramount",
+  genre:"Horror",
+  day_of_the_week:"Saturday",
+  time:"4:30 PM",
+  image:"dracula.jpg"
+},
+{
+  id:23,
+  title:"Attila (Attila the Hun)",
+  director_last_name:"Sirl",
+  company_name:"Pixar",
+  genre:"Action|Adventure| Drama|War",
+  day_of_the_week:"Tuesday",
+  time:"5:30 PM",
+  image:"atila.jpg"
+},
+{
+  id:24,
+  title:"Inception",
+  director_last_name:"Winning",
+  company_name:"Disney",
+  genre:"Comedy|Mystery",
+  day_of_the_week:"Monday",
+  time:"5:30 PM",
+  image:"inception.jpg"
+},
+{
+  id:25,
+  title:"Valentin (Valentín)",
+  director_last_name:"Houseman",
+  company_name:"Universal Pictures",
+  genre:"Drama",
+  day_of_the_week:"Thursday",
+  time:"6:00 PM",
+  image:"Valentin.jpg"
+},
+{
+  id:26,
+  title:"Chhoti Si Baat",
+  director_last_name:"Allsebrook",
+  company_name:"Paramount",
+  genre:"Comedy|Romance",
+  day_of_the_week:"Sunday",
+  time:"6:00 PM",
+  image:"chhoti.jpg"
+},
+{
+  id:27,
+  title:"My Science Project",
+  director_last_name:"Hansberry","company_name":"Universal Pictures",
+  genre:"Adventure|Sci-Fi",
+  day_of_the_week:"Wednesday",
+  time:"6:00 PM",
+  image:"science-project.jpg"
+},
+{
+  id:28,
+  title:"Corner Gas: The Movie",
+  director_last_name:"Balham",
+  company_name:"Universal Pictures",
+  genre:"Comedy",
+  day_of_the_week:"Sunday",
+  time:"6:30 PM",
+  image:"corner-gas.jpg"
+},
+{
+  id:29,
+  title:"Lesson of the Evil (Aku no kyôten)",
+  director_last_name:"Hains",
+  company_name:"Paramount",
+  genre:"Thriller",
+  day_of_the_week:"Tuesday",
+  time:"6:30 PM",
+  image:"lesson-evil.jpg"
+},
+{
+  id:30,
+  title:"Presenting Lily Mars",
+  director_last_name:"Callard",
+  company_name:"Sony Pictures",
+  genre:"Comedy|Musical| Romance",
+  day_of_the_week:"Tuesday",
+  time:"6:30 PM",
+  image:"presenting-lily.jpg"
+},
+{
+  id:31,
+  title:"Defenders of Riga",
+  director_last_name:"Robart",
+  company_name:"Sony Pictures",
+  genre:"Action|Drama|War",
+  day_of_the_week:"Saturday",
+  time:"6:30 PM",
+  image:"defenders.jpg"
+},
+{
+  id:32,
+  title:"Simon Magus",
+  director_last_name:"Merrgan",
+  company_name:"Warner Bros.",
+  genre:"Drama|Fantasy| Mystery|Romance",
+  day_of_the_week:"Sunday",
+  time:"7:00 PM",
+  image:"simon-magus.jpg"
+},
+{
+  id:33,
+  title:"Sharpe's Gold",
+  director_last_name:"Robic",
+  company_name:"Universal Pictures",
+  genre:"Action|Adventure|War",
+  day_of_the_week:"Monday",
+  time:"7:00 PM",
+  image:"sharpes-gold.jpg"
+},
+{
+  id:34,
+  title:"By the Sword",
+  director_last_name:"Skeete",
+  company_name:"Sony Pictures",
+  genre:"Drama",
+  day_of_the_week:"Saturday",
+  time:"7:00 PM",
+  image:"by-the-sword.jpg"
+},
+{
+  id:35,
+  title:"Sister Street Fighter (Onna hissatsu ken)",
+  director_last_name:"Atack",
+  company_name:"Sony Pictures",
+  genre:"Action|Drama",
+  day_of_the_week:"Tuesday",
+  time:"7:30 PM",
+  image:"sister-street-fighter.jpg"
+},
+{
+  id:36,
+  title:"The Swedish Moment",
+  director_last_name:"Arthey",
+  company_name:"Paramount",
+  genre:"Comedy",
+  day_of_the_week:"Friday",
+  time:"8:00 PM",
+  image:"swedish-moment.jpg"
+},
+{
+  id:37,
+  title:"Birdcage",
+  director_last_name:"Farnin",
+  company_name:"Sony Pictures",
+  genre:"Comedy",
+  day_of_the_week:"Saturday",
+  time:"8:00 PM",
+  image:"birdcage.jpg"
+},
+{
+  id:38,
+  title:"Torch Song Trilogy",
+  director_last_name:"Baildon",
+  company_name:"Warner Bros.",
+  genre:"Comedy|Drama|Romance",
+  day_of_the_week:"Wednesday",
+  time:"8:00 PM",
+  image:"torch-song.jpg"
+},
+{
+  id:39,
+  title:"Pickpocket",
+  director_last_name:"Calrow",
+  company_name:"Paramount",
+  genre:"Crime|Drama",
+  day_of_the_week:"Thursday",
+  time:"8:00 PM",
+  image:"pickpocket.jpg"
+},
+{
+  id:40,
+  title:"Redemption: The Stan Tookie Williams Story",
+  director_last_name:"Milmo",
+  company_name:"Paramount",
+  genre:"Crime|Documentary| Drama",
+  day_of_the_week:"Wednesday",
+  time:"8:30 PM",
+  image:"redemption.jpg"
+},
+{
+  id:41,
+  title:"Sherlock Holmes and the Masks of Death",
+  director_last_name:"Whitesel",
+  company_name:"Universal Pictures",
+  genre:"Mystery",
+  day_of_the_week:"Wednesday",
+  time:"8:30 PM",
+  image:"sherlock.jpg"
+},
+{
+  id:42,
+  title:"Abendland",
+  director_last_name:"Cracoe",
+  company_name:"Universal Pictures",
+  genre:"Documentary",
+  day_of_the_week:"Saturday",
+  time:"9:00 PM",
+  image:"abendland.jpg"
+},
+{
+  id:43,
+  title:"Dark Wind",
+  director_last_name:"Leagas",
+  company_name:"Disney",
+  genre:"Action|Mystery",
+  day_of_the_week:"Sunday",
+  time:"9:00 PM",
+  image:"dark-wind.jpg"
+},
+{
+  id:44,
+  title:"Boyz N the Hood",
+  director_last_name:"Whitemarsh",
+  company_name:"Sony Pictures",
+  genre:"Crime|Drama",
+  day_of_the_week:"Wednesday",
+  time:"9:00 PM",
+  image:"boyz.jpg"
+},
+{
+  id:45,
+  title:"American Virgin",
+  director_last_name:"Acors",
+  company_name:"Universal Pictures",
+  genre:"Comedy",
+  day_of_the_week:"Friday",
+  time:"10:00 PM",
+  image:"american-virgin.jpg"
+},
+{
+  id:46,
+  title:"Quintet",
+  director_last_name:"Cremen",
+  company_name:"Pixar",
+  genre:"Mystery|Sci-Fi",
+  day_of_the_week:"Wednesday",
+  time:"10:00 PM",
+  image:"quintet.jpg"
+},
+{
+  id:47,
+  title:"Oscar and the Lady in Pink",
+  director_last_name:"Thorneywork",
+  company_name:"Paramount",
+  genre:"Drama",
+  day_of_the_week:"Tuesday",
+  time:"10:30 PM",
+  image:"oscar-pink.jpg"
+},
+{
+  id:48,
+  title:"Dollman",
+  director_last_name:"MacEveley",
+  company_name:"Paramount",
+  genre:"Action|Comedy|Crime",
+  day_of_the_week:"Tuesday",
+  time:"10:30 PM",
+  image:"dollman.jpeg"
+},
+{
+  id:49,
+  title:"One Small Hitch",
+  director_last_name:"Gouinlock",
+  company_name:"Warner Bros.",
+  genre:"Comedy|Romance",
+  day_of_the_week:"Thursday",
+  time:"10:30 PM",
+  image:"one-small-hitch.jpg"
+},
+{
+  id:50,
+  title:"Nomad (Köshpendiler)",
+  director_last_name:"Maffione",
+  company_name:"Disney",
+  genre:"Drama|War",
+  day_of_the_week:"Tuesday",
+  time:"10:30 PM",
+  image:"nomad.jpg"
+},
+
+
+
+
+
+
+]
+
+export default mock;
