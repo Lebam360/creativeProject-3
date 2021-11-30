33064f41b452a254dd94292f8cd62a05d6847671
Alexa Urrea <alexa.urrea98.com>
2021-11-30 13:07:39 -0700

    Created Favorites page

---
+++ creative-project3/front-end/src/views/Favorites.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="about">
+    <h1 class="title">Favorites</h1>
+    <MovieList :movies="movies" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'Admin',
+  data() {
+    return {
+      title: "",
+      description: "",
+      file: null,
+      addItem: null,
+      items: [],
+      findTitle: "",
+      findItem: null,
+    }
+  },
+  computed: {
+    suggestions() {
+      let items = this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+      return items.sort((a, b) => a.title > b.title);
+    }
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          title: this.title,
+          description: this.description,
+          path: r1.data.path
+        });
+        this.addItem = r2.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    selectItem(item) {
+      this.findTitle = "";
+      this.findItem = item;
+    },
+    async deleteItem(item) {
+      try {
+        await axios.delete("/api/items/" + item._id);
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async editItem(item) {
+      try {
+        await axios.put("/api/items/" + item._id, {
+          title: this.findItem.title,
+          description: this.findItem.description,
+        });
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+
+}
+</script>
---
+++ creative-project3/src/components/MovieList.vue
@@ -0,0 +1,106 @@
+<template>
+<div class="wrapper">
+  <div class="movies">
+    <div class="movie" v-for="movie in movies" :key="movie.id">
+      <div class="image">
+        <img :src="'/images/posters/'+movie.image">
+      </div>
+      <div class="info">
+        <h1>{{movie.title}}</h1>
+        <h6>{{movie.company_name}}</h6>
+        <h5>{{movie.genre}}</h5>
+      </div>
+      <div class="time">
+        <h2>{{movie.day_of_the_week}}</h2>
+        <h2>{{movie.time}}</h2>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MovieList',
+  props: {
+    movies: Array
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.movies {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.movie {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
+
+.movie img {
+  border: 2px solid #333;
+  height: 250px;
+  width: 200px;
+  object-fit: cover;
+}
+
+.movie .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+
+.info {
+  background-color: #800000;
+  color: #bacbdd;
+  padding: 10px 30px;
+  height: 220px;
+}
+
+.info h1 {
+  font-size: 16px;
+}
+
+.info h2 {
+  font-size: 14px;
+}
+
+.info p {
+  font-size: 12px;
+}
+
+.time h2 {
+  margin-right: 20px;
+}
+
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+.time {
+  display: flex;
+}
+
+button {
+  height: 50px;
+  background: #000;
+  color: white;
+  border: none;
+}
+
+.auto {
+  margin-left: auto;
+}
+</style>
